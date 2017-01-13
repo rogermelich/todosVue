11d9a1c197b80324ca05e4fd6515c339da27a48f
Roger Melich <rogermelich@gmail.com>
2017-01-13 16:23:34 +0100

    Add Vue Material and configured Sidenav

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-    <sdsldsdksldk></sdsldsdksldk>
-  </div>
+  <div class="phone-viewport">
+    <md-toolbar>
+      <md-button class="md-icon-button" @click="toggleLeftSidenav">
+        <md-icon>menu</md-icon>
+      </md-button>
+
+      <h2 class="md-title">todoBackend</h2>
+    </md-toolbar>
+
+    <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Sidenav content</h3>
+        </div>
+      </md-toolbar>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi cupiditate esse necessitatibus beatae nobis, deserunt ut est fugit, tempora deleniti, eligendi commodi doloribus. Nemo, assumenda possimus, impedit inventore perferendis iusto!</p>
+    </md-sidenav>
+    </div>
 </template>
 
 <script>
@@ -11,6 +27,17 @@ export default {
   name: 'app',
   components: {
     Hello
+  },
+  methods: {
+    toggleLeftSidenav  () {
+      this.$refs.leftSidenav.toggle()
+    },
+    open (ref) {
+      console.log('Opened: ' + ref)
+    },
+    close (ref) {
+      console.log('Closed: ' + ref)
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+
+Vue.use(VueMaterial)
 
 /* eslint-disable no-new */
 new Vue({
