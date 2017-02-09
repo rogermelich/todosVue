e900faf33f268151596e1f49d3c04c50d59f5b7f
Roger Melich <rogermelich@gmail.com>
2017-02-09 21:50:45 +0100

    Finaly Modification Profile

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,34 +1,89 @@
 <template>
-    <div>
-        <md-card md-with-hover>
-            <md-card-header>
-                <md-avatar>
-                    <img src="{{ gravatar }}" alt="People">
-                </md-avatar>
+    <md-card md-with-hover>
+        <md-card-header>
+            <md-avatar>
+                <img :src="avatar" alt="Roger Melich Curto">
+            </md-avatar>
 
-                <div class="md-title">{{ name }}</div>
-                <div class="md-subhead">Subtitle here</div>
-            </md-card-header>
+            <div class="md-title">{{ id }} {{ name }}</div>
+            <div class="md-subhead">{{ email}}</div>
+        </md-card-header>
 
-            <md-card-content>
-                Lorem ipsum dolor sit amet, consecteturdipisicing elit. Optio itaque ea nostrum.
-            </md-card-content>
-        </md-card>
-    </div>
+        <md-card-content>
+            <md-spinner :md-size="150" md-indeterminate  class="md-accent" v-show="connecting" ></md-spinner>
+            <form novalidate @submit.stop.prevent="submit">
+                <md-input-container>
+                    <label>Name</label>
+                    <md-input v-model="name" placeholder="Put your name here"></md-input>
+                </md-input-container>
+
+                <md-input-container>
+                    <label>Email</label>
+                    <md-input v-model="email" placeholder="Put your email here"></md-input>
+                </md-input-container>
+
+                <md-input-container>
+                    <label>Created at</label>
+                    <md-input v-model="createdAt" placeholder="Date here"></md-input>
+                </md-input-container>
+
+                <md-input-container>
+                    <label>Updated at</label>
+                    <md-input v-model="updatedAt" placeholder="Date here"></md-input>
+                </md-input-container>
+            </form>
+        </md-card-content>
+
+        <md-card-actions>
+            <md-button>Edit</md-button>
+            <md-button>Delete</md-button>
+        </md-card-actions>
+        <md-snackbar md-position="bottom center" ref="connectionError" md-duration="4000">
+            <span>Connection error. Please reconnect using connect button!.</span>
+        </md-snackbar>
+    </md-card>
 </template>
 <style>
-
 </style>
 <script>
-export default {
-    data () {
-        return {
-            avatar: 'https://s.gravatar.com/avatar/52fb84f3e6d38e577b9bef8c412b01a7?s=80',
-            name: 'Roger Melich'
-        }
+import todosVue from '../todosVue'
+export default{
+  data () {
+    return {
+      avatar: 'https://s.gravatar.com/avatar/52fb84f3e6d38e577b9bef8c412b01a7?s=80',
+      id: null,
+      name: null,
+      email: null,
+      createdAt: null,
+      updatedAt: null,
+      connecting: true
+    }
+  },
+  computed: {
+    avatarHash: function () {
+      return '52fb84f3e6d38e577b9bef8c412b01a7'
+    }
+  },
+  created () {
+    this.fetchUserProfile()
+  },
+  methods: {
+    fetchUserProfile: function () {
+      this.$http.get(todosVue.API_PROFILE_URL).then((response) => {
+        this.connecting = false
+        this.id = response.data.id
+        this.name = response.data.name
+        this.email = response.data.email
+        this.createdAt = response.data.created_at
+        this.updatedAt = response.data.updated_at
+      }, (response) => {
+        this.connecting = false
+        this.showConnectionError()
+      })
     },
-    create () {
-        console.log('Component Profile created')
-           this.fetchDate()
+    showConnectionError () {
+      this.$refs.connectionError.open()
+    }
+  }
 }
 </script>
