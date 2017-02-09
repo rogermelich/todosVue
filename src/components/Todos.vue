6c229c8c7d856666afc7e605c9782087550ec147
Roger Melich <rogermelich@gmail.com>
2017-02-09 19:47:42 +0100

    Add API URL

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -11,6 +11,19 @@
                 {{ todo.name }}
             </li>
         </ul>
+
+        <md-snackbar md-position="bottom center" ref="connectionError" md-duration="4000">
+            <span>Connection error. Please reconnect using connect button!.</span>
+        </md-snackbar>
+
+        <md-dialog-confirm
+                md-title="Logout"
+                md-content="Are you sure you want to logout?"
+                md-ok-text="Ok"
+                md-cancel-text="Cancel"
+                @close="onCloseSureToLogout"
+                ref="sureToLogout">
+        </md-dialog-confirm>
     </div>
 </template>
 <style>
@@ -20,16 +33,19 @@
 var STORAGE_KEY = 'todosvue_token'
 var AUTH_CLIENT_ID = 4
 var AUTH_REDIRECT_URI = 'http://localhost:8095/todos'
+var API_URL = 'http://todos.dev:8080/api/v1/task'
+var OAUTH_SERVER_URL = 'http://todos.dev:8080/oauth/authorize?'
 
 export default{
   data () {
     return {
       todos: [],
-      authorized: false
+      authorized: false,
+      token: null
     }
   },
   created () {
-    var token = this.extractToken(document.location.hash)
+    if (document.location.hash) var token = this.extractToken(document.location.hash)
     if (token) this.saveToken(token)
     if (!this.token == null) this.token = this.fetchToken()
     if (this.fetchToken()) {
