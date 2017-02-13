93aa6a362e7775910026a32bf22d0ecd7a90893a
Roger Melich <rogermelich@gmail.com>
2017-02-13 12:50:14 +0100

    Add routes and auth

--- src/services/router.js
+++ src/services/router.js
@@ -1,3 +1,17 @@
-/**
- * Created by roger on 9/02/17.
- */
+import VueRouter from 'vue-router'
+import routes from './routes'
+import auth from './auth'
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.auth && !auth.loggedIn()) {
+    return next('/login')
+  }
+  next()
+})
+
+export default router
--- src/services/routes.js
+++ src/services/routes.js
@@ -1,14 +1,37 @@
 /**
  * Created by roger on 27/01/17.
  */
-import Todos from 'components/Todos.vue'
-import Tokens from 'components/Tokens.vue'
-import Profile from 'components/Profile.vue'
+import Todos from '../components/Todos'
+import Tokens from '../components/Tokens'
+import Profile from '../components/Profile'
+import Login from '../components/Login'
+import NotFound from '../components/NotFound'
 
 const routes = [
-    { path: '/todos', component: Todos },
-    { path: '/tokens', component: Tokens },
-    { path: '/profile', component: Profile }
+  { path: '/',
+    component: Todos,
+    meta: { auth: true }
+  },
+  { path: '/todos',
+    component: Todos,
+    meta: { auth: true }
+  },
+  { path: '/tokens',
+    component: Tokens,
+    meta: { auth: false }
+  },
+  { path: '/profile',
+    component: Profile,
+    meta: { auth: true }
+  },
+  { path: '/login',
+    component: Login,
+    meta: { auth: false }
+  },
+  { path: '*',
+    component: NotFound,
+    meta: { auth: false }
+  }
 ]
 
 export default routes
