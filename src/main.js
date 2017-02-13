ade14cd78e5f0b0e3e4a219b110b1db01311a9fa
Roger Melich <rogermelich@gmail.com>
2017-02-13 12:51:52 +0100

    import auth and router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// The Vue build version to load with the `import` command
+// The Vue build version to ºoad with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
@@ -8,25 +8,23 @@ import 'vue-material/dist/vue-material.css'
 import VueRouter from 'vue-router'
 import Axios from 'axios'
 import querystring from 'querystring'
+import auth from './services/auth'
 
 window.axios = Axios
 window.querystring = querystring
 Vue.prototype.$http = Axios
 
-import Todos from 'components/Todos.vue'
-import Tokens from 'components/Tokens.vue'
-import Profile from 'components/Profile.vue'
-import routes from 'routes.js'
+window.axios.defaults.headers.common = {
+  'X-Requested-With': 'XMLHttpRequest'
+}
 
-// const routes = [
-//   { path: '/todos', component: Todos },
-//   { path: '/tokens', component: Tokens },
-// ]
+if (auth.loggedIn()) {
+  window.axios.defaults.headers.common = {
+    'Authorization': auth.getAuthHeader()
+  }
+}
 
-const router = new VueRouter({
-  mode: 'history',
-  routes
-})
+import router from './services/router'
 
 Vue.use(VueMaterial)
 Vue.use(VueRouter)
