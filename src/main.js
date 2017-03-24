d39c9e753b1f29d8e0cb1ef8cb3833fd557b501b
Roger Melich <rogermelich@gmail.com>
2017-03-24 17:07:49 +0100

    add material icons and roboto

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import VueMaterial from 'vue-material'
 
+import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
+
+import 'material-design-icons/iconfont/material-icons.css'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+
 import VueRouter from 'vue-router'
 import Axios from 'axios'
 import querystring from 'querystring'
